544e257cef87a033ed59f4cf884be1541f2084ec
Orest Kostiuk <okostiuk@assurance.com>
2023-03-22 10:48:26 +0100

    added chatgpt

---
+++ app/services/chatgpt_service.rb
@@ -0,0 +1,36 @@
+class ChatgptService
+  include HTTParty
+
+  attr_reader :api_url, :options, :model, :message
+
+  def initialize(message, model = 'gpt-3.5-turbo')
+    api_key = Rails.application.credentials.chatgpt_api_key
+    @options = {
+      headers: {
+        'Content-Type' => 'application/json',
+        'Authorization' => "Bearer #{'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'}"
+      }
+    }
+    @api_url = 'https://api.openai.com/v1/chat/completions'
+    @model = model
+    @message = message
+  end
+
+  def call
+    body = {
+      model: @model,
+      messages: [{ role: 'user', content: message }]
+    }
+    response = HTTParty.post(api_url, body: body.to_json, headers: options[:headers])
+    raise response['error']['message'] unless response.code == 200
+
+    response['choices'][0]['message']['content']
+  end
+
+  class << self
+    def call(message, model = 'gpt-3.5-turbo')
+      new(message, model).call
+    end
+  end
+end
+
